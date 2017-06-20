35ec86bc39ffccdfc1db116d555d55ddd9abb99d
Toph Wells <toph.wells@gmail.com>
2017-06-20 21:37:03 +0200

    now generate a simple map using the built-in cellular rules

--- game.js
+++ game.js
@@ -2,9 +2,13 @@ var Game = {
     display: null,
  
     init: function() {
+		var w = 60;
+		var h = 40;
+		
+		//perform canvas setup
 		var options = {
-			width: 60,
-			height: 40,
+			width: w,
+			height: h,
 			fontSize: 14,
 			forceSquareRatio:false
 		}
@@ -25,8 +29,45 @@ var Game = {
         this.display = new ROT.Display(options);
         container.appendChild(this.display.getContainer());
 		
+		//setup map generation
+		this.map = [];
+		for (var x = 0; x < w; x++)
+		{
+			this.map[x] = []; 
+		}
+		var mapCallback = function(x, y, value) {
+        this.map[x][y] = value;
+		}
+		this.generateMap = function() {
+			var cellular = new ROT.Map.Cellular(w, h,{topology: 4, born:[3,4], survive: [2,3,4]});
+			cellular.randomize(0.5);
+			cellular.create();
+			cellular.create();
+			cellular.create();
+			cellular.connect();
+			cellular.serviceCallback(mapCallback.bind(this));
+		}
+		this.drawWholeMap = function() {
+			for (var x = 0; x < w; x++)
+			{
+				for (var y = 0; y < h; y++)
+				{
+					if (this.map[x][y]) //wall
+					{
+						this.display.draw(x, y, " ", "#000000", "#ffffff");
+					}
+					else //floor
+					{
+						this.display.draw(x, y, ".", "#ffffff", "#000000");
+					}
+				}
+			}
+		}
+		
+		//generate map
+		this.generateMap();
+		this.drawWholeMap();
 		
 		
-		this.display.drawText(20, 25, "%c{#00ff00}Congratulations!%c{} The game has started! But it's not doing anything yet.", 20);
     }
 }
